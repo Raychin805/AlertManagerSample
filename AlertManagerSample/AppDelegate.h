4aaf9c5c46128bac66412b4cc34d75778103921b
Raychin805 <raychin805@gmail.com>
2017-10-31 14:52:50 +0800

    Init Xcode Project

---
+++ AlertManagerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertManagerSample
+//
+//  Created by raymond on 2017/10/31.
+//  Copyright © 2017年 Raymond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
